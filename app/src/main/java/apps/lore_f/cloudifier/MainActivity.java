d42ed187b4b3205a87f1ca1bd29b0044f2127307
lorenzofailla <lorenzofailla.developer@gmail.com>
2017-06-01 18:12:04 +0200

    -

---
+++ app/src/main/java/apps/lore_f/cloudifier/MainActivity.java
@@ -0,0 +1,37 @@
+package apps.lore_f.cloudifier;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* inizializza il FirebaseAuth */
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        /* ottiene l'user corrente */
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser == null) {
+            // autenticazione non effettuata
+
+            // lancia la SignInActivity e termina l'attività corrente
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+
+        }
+
+    }
+}
